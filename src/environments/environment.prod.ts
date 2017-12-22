e2d0428fd1188ff68b05c1403f2092b4e5a097e5
Kyle Richards <kyleLRichards11@gmail.com>
2017-12-22 11:54:22 -0500

    firebase init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCSJ-jAkye3uTC2T9DfjhlsL1Hz_78V9sE",
+    authDomain: "nfl-picks-b5e4d.firebaseapp.com",
+    databaseURL: "https://nfl-picks-b5e4d.firebaseio.com",
+    projectId: "nfl-picks-b5e4d",
+    storageBucket: "nfl-picks-b5e4d.appspot.com",
+    messagingSenderId: "348465044323"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCSJ-jAkye3uTC2T9DfjhlsL1Hz_78V9sE",
+    authDomain: "nfl-picks-b5e4d.firebaseapp.com",
+    databaseURL: "https://nfl-picks-b5e4d.firebaseio.com",
+    projectId: "nfl-picks-b5e4d",
+    storageBucket: "nfl-picks-b5e4d.appspot.com",
+    messagingSenderId: "348465044323"
+  }
 };
